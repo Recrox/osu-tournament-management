97d8624eaa644ea403905a085228af7ee65c689c
Arno Leruste <recroX91@hotmail.com>
2022-08-07 10:58:45 +0200

    fix bugs and add some button and form

--- OsuToolForm.Designer.cs
+++ OsuToolForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace osu_tournament_management
 {
-    partial class Form1
+    partial class OsuToolForm
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OsuToolForm));
             this.button1 = new System.Windows.Forms.Button();
             this.listBoxTeam = new System.Windows.Forms.ListBox();
             this.listBoxPlayer = new System.Windows.Forms.ListBox();
@@ -35,11 +36,14 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(100, 538);
+            this.button1.Location = new System.Drawing.Point(45, 538);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -75,7 +79,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(413, 538);
+            this.button2.Location = new System.Drawing.Point(357, 538);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
@@ -85,7 +89,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(739, 538);
+            this.button3.Location = new System.Drawing.Point(674, 538);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 5;
@@ -103,11 +107,44 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "Osu Tournament Management";
             // 
-            // Form1
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(166, 537);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 24);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "DeleteTeam";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.DeleteTeam_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(482, 537);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(82, 24);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "DeletePlayer";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.DeletePlayer_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(799, 537);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(82, 24);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "DeleteScore";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.DeleteScore_Click);
+            // 
+            // OsuToolForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -115,7 +152,8 @@
             this.Controls.Add(this.listBoxPlayer);
             this.Controls.Add(this.listBoxTeam);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "OsuToolForm";
             this.Text = "Osu_tool";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -131,6 +169,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- OsuToolForm.cs
+++ OsuToolForm.cs
@@ -10,47 +10,47 @@ using System.Windows.Forms;
 
 namespace osu_tournament_management
 {
-    public partial class Form1 : Form
+    public partial class OsuToolForm : Form
     {
         private List<Team> Teams;
-        public Form1()
+        public OsuToolForm()
         {
             InitializeComponent();
 
             Teams = new List<Team>();
-            testListBoxs();
-            
+            FillModel();
+            ShowTeams();
 
         }
 
-        private void testListBoxs()
+        private void FillModel()
         {
-            //models
             for (int i = 0; i < 3; i++)
             {
                 Teams.Add(new Team("Team num " + i));
                 for (int j = 0; j < 3; j++)
                 {
-                    Teams[i].AddPlayer("Joueur num " + j + " "+i);
+                    Teams[i].AddPlayer("Joueur num " + j + " " + i);
                     for (int k = 0; k < 10; k++)
                     {
-                        Teams[i].Players[j].AddScore("NM" + k+" "+j+" "+i, k * 100000);
-                    } 
+                        Teams[i].Players[j].AddScore("NM" + k + " " + j + " " + i, k * 100000);
+                    }
                 }
+            }       
+        }
 
-            }
-            //views
+        private void ShowTeams()
+        {
+            listBoxTeam.Items.Clear();
+            listBoxPlayer.Items.Clear();
+            listBoxScore.Items.Clear();
+
             foreach (Team team in Teams)
             {
                 listBoxTeam.Items.Add(team.Name);
             }
-
-            listBoxTeam_SelectedValueChanged(null, null);
-            listBoxPlayer_SelectedValueChanged(null, null);
-
         }
 
-        
         private Team getTeamSelected()
         {
             return Teams.ElementAt(listBoxTeam.SelectedIndex);
@@ -61,23 +61,29 @@ namespace osu_tournament_management
         }
         private void AddTeam_Click(object sender, EventArgs e)
         {
-            
+            new AddDataForm().ShowDialog();
         }
 
         private void AddPlayer_Click(object sender, EventArgs e)
         {
-
+            new AddDataForm().ShowDialog();
         }
         private void AddScore_Click(object sender, EventArgs e)
         {
-
+            new AddDataForm().ShowDialog();
         }
 
         private void listBoxTeam_SelectedValueChanged(object sender, EventArgs e)
         {
+            showPlayerOfTheTeamSelect();
+        }
+
+        private void showPlayerOfTheTeamSelect()
+        {
             if (listBoxTeam.SelectedItem != null)
             {
                 listBoxPlayer.Items.Clear();
+                listBoxScore.Items.Clear();
                 foreach (Player player in getTeamSelected().Players)
                 {
                     listBoxPlayer.Items.Add(player.Name);
@@ -87,6 +93,11 @@ namespace osu_tournament_management
 
         private void listBoxPlayer_SelectedValueChanged(object sender, EventArgs e)
         {
+            showScoreOfThePlayerSelect();
+        }
+
+        private void showScoreOfThePlayerSelect()
+        {
             if (listBoxPlayer.SelectedItem != null)
             {
                 listBoxScore.Items.Clear();
@@ -95,6 +106,28 @@ namespace osu_tournament_management
                     listBoxScore.Items.Add(score.Mod + " " + score.Point);
                 }
             }
+        }
+
+        private void DeleteTeam_Click(object sender, EventArgs e)
+        {
+            if (listBoxTeam.SelectedItem != null)
+                this.Teams.RemoveAt(listBoxTeam.SelectedIndex);
+                ShowTeams();
+        }
+
+        private void DeletePlayer_Click(object sender, EventArgs e)
+        {
+            if (listBoxPlayer.SelectedItem != null)
+                getTeamSelected().Players.RemoveAt(listBoxPlayer.SelectedIndex);
+                showPlayerOfTheTeamSelect();
+
+        }
+
+        private void DeleteScore_Click(object sender, EventArgs e)
+        {
+            if (listBoxScore.SelectedItem != null)
+                getPlayerSelected().Scores.RemoveAt(listBoxScore.SelectedIndex);
+                showScoreOfThePlayerSelect();
         }
     }
 }
